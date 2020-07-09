1cc7c0376bd53d563ddc627e46be3a13253b3bd3
bcy <bcygsws@163.com>
2020-07-09 18:00:22 +0800

    React框架-评论列表案例实现

--- src/component/comment/CmtBox.jsx
+++ src/component/comment/CmtBox.jsx
@@ -6,6 +6,41 @@ export default class CmtBox extends React.Component {
     this.state = {};
   }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <label>评论人：</label>
+        <br />
+        <input type="text" ref="user" />
+        <br />
+        <label>评论内容：</label>
+        <br />
+        <textarea cols="30" rows="6" ref="content"></textarea>
+        <br />
+        <input
+          type="button"
+          value="发表评论"
+          onClick={this.submitComment.bind(this)}
+        />
+      </div>
+    );
+  }
+  submitComment() {
+    // 1.获取评论人和评论内容
+    // 在jQuery中有text()方法和val()方法来获取值，但是注意text()方法只能获取textarea的初始值，textarea中内容变化后就
+    // 获取不到了，需要使用val()来获取。综上，js中获取textarea内容值最好使用value属性，而jQuery中用val()方法来获取
+    var cmtInfo = {
+      user: this.refs.user.value,
+      content: this.refs.content.value,
+    };
+    // 2.从本地存储中获取之前的评论数组,短路运算第二个操作数，表示没有存储内容时
+    var list = JSON.parse(localStorage.getItem('cmt')) || [];
+    // 3.把本条数据unshift原数组前面去
+    list.unshift(cmtInfo);
+    // 4.把新数组存放到本地存储localStorage中
+    localStorage.setItem('cmt', JSON.stringify(list));
+    // 5.清空文本框和多行文本输入框中的内容，方便下次输入内容
+    this.refs.user.value = this.refs.content.value = '';
+    // 每次添加完新内容，就重新更新组件
+    this.props.reload();
   }
 }
--- src/component/comment/CmtList.jsx
+++ src/component/comment/CmtList.jsx
@@ -7,21 +7,35 @@ export default class CMTList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [
-        { user: '张三', content: '123' },
-        { user: '李四', content: '1232243' },
-        { user: '小红', content: '123ee' },
-      ],
+      // list: [
+      //   { user: '张三', content: '123' },
+      //   { user: '李四', content: '1232243' },
+      //   { user: '小红', content: '123ee' },
+      // ],
+      // 不把数据写死，让其从本地存储中获得
     };
   }
+  // 该组件挂载在页面前，就完成对数据的初始化
+  // 但是这样做是不够的，初始化阶段是可以很好的完成本地存储（key="cmt"）的渲染，但是再在页面中添加1条信息，需要手动刷新页面后才能看到。为此，我们需要在点击按钮（在子组件中）后，就立即刷新内容。涉及到父子组件之间的传值了
+  componentWillMount() {
+    this.initState();
+  }
+  initState = () => {
+    this.setState({
+      list: JSON.parse(localStorage.getItem('cmt')) || [],
+    });
+  };
   render() {
     return (
       <div>
         <h1>这是评论列表组件</h1>
         {/* 发表评论的组件 */}
-        <CmtBox></CmtBox>
+        {/* CmtList组件向子组件CmtBox传值，以自定义属性的方式传值。在CmtBox中拿到属性值后调用即可。
+        1.在Vue中，父组件向子组件传值，对于子组件属性使用props:[]来接收。而对于父组件传递给组件的方法，则使用this.$emit('方法名')来接收。
+        2.而在React中父组件传递给子组件的属性和方法，子组件都是以属性的方式this.props.*来接收，更简单一些 */}
+        <CmtBox reload={this.initState}></CmtBox>
         {/* 循环渲染内容的组件---注意渲染循环列表，使用map方法 */}
-        {this.state.list.map((item,index) => {
+        {this.state.list.map((item, index) => {
           return <CmtItem {...item} key={index}></CmtItem>;
         })}
       </div>
